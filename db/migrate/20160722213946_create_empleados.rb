58fc0c34dfc615175fc028968a422022dff6bf2e
Mario Miranda <mario.miranda.luengo@gmail.com>
2016-07-22 18:53:51 -0400

    Se inicio el proyecto y se hizo un scaffold de empleados y se agrego un login con devise

---
+++ db/migrate/20160722213946_create_empleados.rb
@@ -0,0 +1,11 @@
+class CreateEmpleados < ActiveRecord::Migration
+  def change
+    create_table :empleados do |t|
+      t.text :name
+      t.integer :age
+      t.string :rut
+
+      t.timestamps null: false
+    end
+  end
+end
